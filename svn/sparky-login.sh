c9b9d556d23f3d177bd2cd23f724779d2473ac4d
Adam <adam@sse.uni-hildesheim.de>
2021-07-26 14:38:32 +0200

    Harden sparky-login.sh by JSON-escaping username and password

--- svn/sparky-login.sh
+++ svn/sparky-login.sh
@@ -8,7 +8,7 @@ code="$(curl \
 	--write-out "%{http_code}" \
 	--request POST "http://sparky-service:8080/api/v1/authenticate" \
 	--header "accept: */*" --header "Content-Type: application/json" \
-	--data "{\"username\":\"$user\",\"password\":\"$password\"}")"
+	--data "{\"username\":$(echo -n "$user" | jq -aRs .),\"password\":$(echo -n "$password" | jq -aRs .)}")"
 
 if [ "$code" = "200" ]
 then
