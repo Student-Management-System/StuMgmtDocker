a370a45972cc80cb577c0f3bdf50e53a9b921863
Adam <adam@sse.uni-hildesheim.de>
2021-07-26 09:31:43 +0200

    Add RightsManagement to svn server

---
+++ svn/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+[[ -d "/repository" ]] || { mkdir "/repository" ; chown -R www-data:www-data "/repository" ; }
+
+pushd /opt/rights-management
+cp settings_template.json settings.json
+
+echo "Preparing settings.json for rights-management"
+sed -i 's|${SVN_COURSE_NAME}|'"${SVN_COURSE##-}"'|g' settings.json
+sed -i 's|${SVN_COURSE_SEMESTER}|'"${SVN_COURSE%-}"'|g' settings.json
+sed -i 's|${SVN_REPO_NAME}|'"$SVN_REPO_NAME"'|g' settings.json
+sed -i 's|${SVN_MGMT_USER}|'"$SVN_MGMT_USER"'|g' settings.json
+sed -i 's|${SVN_MGMT_PW}|'"$SVN_MGMT_PW"'|g' settings.json
+
+mkdir cache
+chown -R www-data:www-data .
+
+echo "Running rights-management"
+su -c "java -Dlog4j.configurationFile=log4j2.yaml -jar rights-management.jar" -s /usr/bin/sh www-data &
+
+popd
+
+echo "Running $@"
+exec "$@"
